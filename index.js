8faba944f9ef024543b0d8fad7b8d16bd76ed8da
Boi <cambodianjesuss@gmail.com>
2023-11-01 21:29:27 -0700

    config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = requires('body-parser');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
-app.use(cors());
+// app.use(cors()); Not required since it will not need to communicate with react
 app.use(bodyParser.json());
 
 
