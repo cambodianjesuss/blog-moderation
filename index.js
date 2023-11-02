6df4eb5c913269f399001c25ddc123883511d283
Boi <cambodianjesuss@gmail.com>
2023-11-01 21:52:51 -0700

    listing on 4003

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ const app = express();
 // app.use(cors()); Not required since it will not need to communicate with react
 app.use(bodyParser.json());
 
+app.post('/events', (req, res)=>{
+  const { type } = req.body;
 
+  if(type === 'CommentUpdated'){
+    // 
+  }
+});
+
+app.listen(4003, ()=>{
+  console.log('Moderation Service on 4003');
+});
