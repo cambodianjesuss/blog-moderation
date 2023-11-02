5b8d47560c4c7dc839617b1a54730a8901bfbcc3
Boi <cambodianjesuss@gmail.com>
2023-11-02 00:40:18 -0700

    recieved an event for comment updated, then checked for word to moderate, then updated the status and emit with commentModerated to the event bus

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { default: axios } = require('axios');
 // const cors = require('cors');
 const app = express();
 
 // app.use(cors()); Not required since it will not need to communicate with react
 app.use(bodyParser.json());
 
-app.post('/events', (req, res)=>{
-  const { type } = req.body;
+app.post('/events', async (req, res)=>{
+  const { type, data } = req.body;
 
   if(type === 'CommentUpdated'){
-    // 
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
   }
+
+  res.send({});
 });
 
 app.listen(4003, ()=>{
